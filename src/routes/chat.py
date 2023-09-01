2b1788ab3732c8406d5116b26e5efbeb8114b08e
Lecrisha Bryan <lecrisha@i-code.codes>
2023-09-01 16:46:58 -0500

    Update chat.py to generate user token using uuid4

--- src/routes/chat.py
+++ src/routes/chat.py
@@ -1,15 +1,26 @@
 import os
-from fastapi import APIRouter, FastAPI, WebSocket, Request
+from fastapi import APIRouter, FastAPI, WebSocket, Request, BackgroundTasks, HTTPException
+import uuid
 
 chat = APIRouter()
 
 # @route    POST /token
-# @desc     Route to generate chat token
+# @desc     Route generating chat token
 # @access   Public
 
 @chat.post("/token")
-async def token_generator(request: Request):
-    return None
+async def token_generator(name: str, request: Request):
+    
+    if name == "":
+        raise HTTPException(status_code=400, detail={
+            "loc": "name", "msg": "Enter a valid name"
+        })
+        
+    token = str(uuid.uuid4())
+    
+    data = {"name": name, "token": token}
+    
+    return data
 
 # @route    POST /refresh_tokens
 # @desc    Route to refresh chat token
